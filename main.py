3f81e722ca9f385f409c073a637be872665b8200
Fluxy <139269530+Fluxy05@users.noreply.github.com>
2025-12-12 15:00:17 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-# logs-automation-python
+import sys
+import os
+
+
+
+fichier_log = open("logA10.log")
+
+line = fichier_log.readline()
+nombre_de_lignes = len(line)
+
+ip = []
+user = []
+date = []
+Type = []
+site = []
+
+for i in range (1, nombre_de_lignes) :
+    line = fichier_log.readline()
+    split = line.split(" ")
+    ip.append(split[0])
+    user.append(split[2])
+    date.append(split[3])
+    Type.append(split[5])
+    site.append(split[10])
+    print(split[10])
+print(ip)
+
