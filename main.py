6775603d1655bab2c02b8ee8ab3d478243d9e321
Fluxy <139269530+Fluxy05@users.noreply.github.com>
2025-12-15 13:21:08 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ date = []
 Type = []
 site = []
 
-for i in range (1, nombre_de_lignes) :
-    line = fichier_log.readline()
+for line in fichier_log :
     split = line.split(" ")
     ip.append(split[0])
     user.append(split[2])
@@ -24,4 +23,15 @@ for i in range (1, nombre_de_lignes) :
     site.append(split[10])
     print(split[10])
 print(ip)
+fichier_log.close
 
+
+if sys.platform == "linux" :
+    os.system("cp header.txt main.html")
+elif sys.platform == "win" :
+    os.system("copy header.txt main.html")
+
+html = open("main.html", "a")
+html.write(" ")
+html.write("<p>Hello</p>")
+html.close
